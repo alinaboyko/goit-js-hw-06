a6209e329e6a50fc4ef75f119895a471f5ce224f
alinaboyko <alinaboyko42@gmail.com>
2023-03-19 23:25:53 +0200

    8-9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget;
+
+    if (email === "" && password === "") {
+        alert("You didn't fill your data :( please try again")
+    };
+    
+    const userData = { email: email.value, password: password.value };
+    console.log(userData);
+
+    event.currentTarget.reset();
+};
+ 
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+"use strict";
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector("button.change-color");
+const spanText = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", (e) => {
+  let color = getRandomHexColor();
+  console.log(color);
+  body.style.backgroundColor = color;
+  spanText.textContent = color;
+});
